d9ec9da22d94be049c2e36a5f993fc846f0659cf
BlackTail2000 <carlosgabrielbacamanrique@hotmail.com>
2024-10-25 22:28:23 -0500

    Proyecto Creado

---
+++ src/main/java/pe/edu/cibertec/app_panchita_reservas_cleaning_clearing/AppPanchitaReservasCleaningClearingApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_panchita_reservas_cleaning_clearing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPanchitaReservasCleaningClearingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPanchitaReservasCleaningClearingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/app_panchita_reservas_cleaning_clearing/AppPanchitaReservasCleaningClearingApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.app_panchita_reservas_cleaning_clearing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPanchitaReservasCleaningClearingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
